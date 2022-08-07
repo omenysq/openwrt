568566a79fa040e50eb5f84cfffdcc8b2fe505f5
Alexandru Gagniuc <mr.nuke.me@gmail.com>
2022-08-07 00:30:23 -0500

    [WIP] ipq60xx: add Qualcomm Atheros IPQ60xx target
    Dirk Buchwalder <buchwalder@posteo.de>: ipq60xx: enable smd rpm driver

---
+++ target/linux/ipq60xx/image/generic.mk
@@ -0,0 +1,32 @@
+define Device/FitImage
+	KERNEL_SUFFIX := -fit-uImage.itb
+	KERNEL = kernel-bin | gzip | fit gzip $$(DEVICE_DTS_DIR)/$$(DEVICE_DTS).dtb
+	KERNEL_NAME := Image
+endef
+
+define Device/FitImageLzma
+	KERNEL_SUFFIX := -fit-uImage.itb
+	KERNEL = kernel-bin | lzma | fit lzma $$(DEVICE_DTS_DIR)/$$(DEVICE_DTS).dtb
+	KERNEL_NAME := Image
+endef
+
+define Device/FitzImage
+	KERNEL_SUFFIX := -fit-zImage.itb
+	KERNEL = kernel-bin | fit none $$(DEVICE_DTS_DIR)/$$(DEVICE_DTS).dtb
+	KERNEL_NAME := zImage
+endef
+
+define Device/UbiFit
+	KERNEL_IN_UBI := 1
+	IMAGES := nand-factory.ubi nand-sysupgrade.bin
+	IMAGE/nand-factory.ubi := append-ubi
+	IMAGE/nand-sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+
+define Device/cp01-c1
+	$(call Device/FitImage)
+	DEVICE_VENDOR := Test
+	DEVICE_MODEL := C3PO
+	SOC := ipq6018
+endef
+TARGET_DEVICES += cp01-c1
